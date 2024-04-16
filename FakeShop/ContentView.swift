64c3bdd2ff18718e8f703febf10683440587a099
JAIRO-99 <jairo.lcelis1999@gmail.com>
2024-04-15 21:37:36 -0500

    Initial Commit

---
+++ FakeShop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeShop
+//
+//  Created by Jairo Laurente Celis on 15/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FakeShop/FakeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeShopApp.swift
+//  FakeShop
+//
+//  Created by Jairo Laurente Celis on 15/04/24.
+//
+
+import SwiftUI
+
+@main
+struct FakeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
